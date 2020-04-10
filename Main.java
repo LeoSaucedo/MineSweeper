bb2217268e2905763d1dc3efb3e024252140d946
Carlos Saucedo <carlos@cgsphoto.com>
2020-04-09 20:12:56 -0400

    Created Main Classes. Completed Tile Class. Started on Main.

---
+++ Main.java
@@ -0,0 +1,35 @@
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import java.awt.GridLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    // Creating frame
+    JFrame frame = new JFrame("Minesweeper");
+
+    // Setting up the menu bar
+    JMenu gameMenu, helpMenu;
+    JMenuBar mb = new JMenuBar();
+    gameMenu = new JMenu("Game");
+    helpMenu = new JMenu("Options");
+    mb.add(gameMenu);
+    mb.add(helpMenu);
+
+    // Instantiating menu and mines panels.
+
+    // Creating the game to display.
+    frame.setJMenuBar(mb);
+    frame.setSize(256, 512);
+    // BorderLayout layout = new BorderLayout();
+    // frame.setLayout(layout);
+    frame.setDefaultCloseOperation(3);
+    // frame.add(menuPanel, BorderLayout.NORTH);
+    MinesPanel minesPanel = new MinesPanel(0);
+    frame.add(minesPanel);
+    frame.setVisible(true);
+  }
+}
---
+++ MinesPanel.java
@@ -0,0 +1,56 @@
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MinesPanel extends JPanel {
+  final int BEGINNER = 0;
+  final int INTERMEDIATE = 1;
+  final int EXPERT = 2;
+
+  public MinesPanel(int difficulty) throws IllegalArgumentException {
+    super();
+    int width, height, mines;
+    // Set difficulty of game.
+    if (difficulty == BEGINNER) {
+      width = 9;
+      height = 9;
+      mines = 10;
+    } else if (difficulty == INTERMEDIATE) {
+      width = 16;
+      height = 16;
+      mines = 40;
+    } else if (difficulty == EXPERT) {
+      width = 30;
+      height = 16;
+      mines = 99;
+    } else {
+      // If the difficulty is invalid.
+      throw new IllegalArgumentException("Invalid difficulty: " + difficulty);
+    }
+    // Create layout of mines.
+    GridLayout layout = new GridLayout(width, height);
+    // 2D array of mines.
+    Tile[][] minesList = new Tile[width][height];
+    // Populating array with blank Tiles.
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        minesList[i][j] = new Tile();
+        this.add(minesList[i][j]);
+      }
+    }
+
+    // Adding mines.
+    int minesPlaced = 0;
+    Random rand = new Random();
+    while (minesPlaced < mines) {
+      int mineX = rand.nextInt(width);
+      int mineY = rand.nextInt(height);
+      if (!minesList[mineX][mineY].isMine()) {
+        minesList[mineX][mineY].setMineState(true);
+        minesPlaced++;
+      }
+    }
+    this.setLayout(layout);
+  }
+}
